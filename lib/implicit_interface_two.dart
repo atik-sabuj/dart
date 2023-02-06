a12ebd90e3b9512f373cc7a5c0c59aa1a02f6b60
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-06 13:05:00 +0600

    Implicit Interface and Implicit Interface : Multiple

---
+++ lib/implicit_interface_two.dart
@@ -0,0 +1,25 @@
+//Implicit Interface : Multiple
+//class class_name implements interface_name1,interface_name2,interface_name3,.......
+void main() {
+  var obj = Daughter();
+  obj.disp1();
+  obj.disp2();
+}
+class Father {
+  disp1() {
+    print('I am Father Class');
+  }
+}
+class Mother {
+  disp2() {
+    print('I am Mother Class');
+  }
+}
+class Daughter implements Father, Mother {
+  disp1() {
+    print('I am Daughter Class 1 From Father');
+  }
+  disp2() {
+    print('I am Daughter Class 2 From Mother');
+  }
+}
