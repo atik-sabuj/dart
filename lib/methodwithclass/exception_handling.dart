3ded6dc3c85770e9da3f469750f5fb59a02fe291
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-09 18:10:14 +0600

    Updated Exception Handling

--- lib/methodwithclass/exception_handling.dart
+++ lib/methodwithclass/exception_handling.dart
@@ -1,4 +1,11 @@
-//Exception Handling
+//Exception Handling:
+/*try {
+// Your Code Here
+}
+catch(ex){
+// Exception here
+}*/
+
 void main() {
   /*var result = 4~/0;
   print(result);*/
@@ -12,6 +19,7 @@ IntegerDivisionByZeroException {
   print('Can not Divided by Zero');
   }*/
 
+
   //try catch
 /*  try {
     var result = 4~/0;
@@ -19,6 +27,20 @@ IntegerDivisionByZeroException {
   } catch(e) {
     print(e);
   }*/
+
+/*  int a = 12;
+  int b = 0;
+  int res;
+  try {
+    res = a ~/ b;
+  } on UnsupportedError {
+    print('Cannot divide by zero');
+  } catch (ex) {
+    print(ex);
+  } finally {
+    print('Finally block always executed');
+  }
+ */
 
   //try on catch
 /*  try {
@@ -30,7 +52,7 @@ IntegerDivisionByZeroException {
       print(e);
     }*/
 
-  //try on catch
+/*  //try on catch
   try {
     var result = 4~/0;
     print(result);
@@ -40,5 +62,5 @@ IntegerDivisionByZeroException {
     print(e);
   } finally {
     print('Finally Always Executes');
-  }
+  }*/
 }
