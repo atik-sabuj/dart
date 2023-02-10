e979bd70a6d02f11826fc645e7dea9ad6340ea60
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-10 15:24:52 +0600

    Updated Set

--- lib/basic/set.dart
+++ lib/basic/set.dart
@@ -19,6 +19,16 @@ print(st5);
 print(st5.runtimeType);*/
 
 
+/*  // declaring fruits as Set
+  Set<String> fruits = {"Apple", "Orange", "Mango", "Banana", "Water Mellon"};
+  // using different properties of Set
+  print("First Value is ${fruits.first}");
+  print("Last Value is ${fruits.last}");
+  print("Is fruits empty? ${fruits.isEmpty}");
+  print("Is fruits not empty? ${fruits.isNotEmpty}");
+  print("The length of fruits is ${fruits.length}");*/
+
+
 //Set : Empty Set
 /*  var st7 = {}; // Map, not set
   print(st7);
@@ -47,6 +57,68 @@ print(p.runtimeType);
 var x= {1,4,3,1,2,3,1,2,7}; //Don't consider duplicate values again
 print(x);
 print(x.runtimeType);*/
+
+
+//Check The Available Value:
+/*  Set<String> fruits = {"Apple", "Orange", "Mango"};
+  print(fruits.contains("Mango"));
+  print(fruits.contains("Lemon"));*/
+
+
+//Add & Remove Items In Set
+/*  Set<String> fruits = {"Apple", "Orange", "Mango"};
+  fruits.add("Lemon");
+  fruits.add("Grape");
+  print("After Adding Lemon and Grape: $fruits");
+  fruits.remove("Apple");
+  print("After Removing Apple: $fruits");*/
+
+
+//Adding Multiple Elements
+/*  Set<int> numbers = {10, 20, 30};
+  numbers.addAll([40,50]);
+  print("After adding 40 and 50: $numbers");*/
+
+
+//Printing All Values In Set
+/*
+  Set<String> fruits = {"Apple", "Orange", "Mango"};
+  for(String fruit in fruits){
+    print(fruit);
+  }
+*/
+
+
+//Clear Set
+/*  Set<String> fruits = {"Apple", "Orange", "Mango"};
+  // to clear all items
+  fruits.clear();
+  print(fruits);*/
+
+
+//Difference In Set:
+/*
+  Set<String> fruits1 = {"Apple", "Orange", "Mango"};
+  Set<String> fruits2 = {"Apple", "Grapes", "Banana"};
+  final differenceSet = fruits1.difference(fruits2);
+  print(differenceSet);
+  final differenceSet2 = fruits2.difference(fruits1);
+  print(differenceSet2);
+*/
+
+
+//Element At Method In Dart
+/*  Set<String> days = {"Sunday", "Monday", "Tuesday"};
+  // index starts from 0 so 2 means Tuesday
+  print(days.elementAt(1));*/
+
+
+//Intersection Method
+/*  Set<String> fruits1 = {"Apple", "Orange","Licchi", "Mango"};
+  Set<String> fruits2 = {"Apple", "Grapes", "Banana","Licchi"};
+  final intersectionSet = fruits1.intersection(fruits2);
+  print(intersectionSet);*/
+
 
 
 //End Of Set
