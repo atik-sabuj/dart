59ec81481c378b8ff516fcac87f0ff18f3a55da5
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-11 12:09:57 +0600

    Updated Class and Object

--- lib/oop/class_object.dart
+++ lib/oop/class_object.dart
@@ -38,6 +38,7 @@ void main(){
 }*/
 
 //Declaring Animal Class Object:
+/*
 class Animal {
   String? name;
   int? numberOfLegs;
@@ -56,4 +57,32 @@ void main() {
   animal.numberOfLegs = 4;
   animal.lifeSpan = 10;
   animal.display();
+}*/
+
+
+//Declaring Car Class Object:
+class Car {
+  String? name;
+  String? color;
+  int? numberOfSeats;
+
+  void start() {
+    print("$name Car Started");
+  }
+}
+
+void main() {
+  //Here car is object of class Car.
+  Car car = Car();
+  car.name = "BMW";
+  car.color = "Red";
+  car.numberOfSeats = 4;
+  car.start();
+
+  //Here car2 is object of class Car.
+  Car car2 = Car();
+  car2.name = "Audi";
+  car2.color = "Black";
+  car2.numberOfSeats = 4;
+  car2.start();
 }
