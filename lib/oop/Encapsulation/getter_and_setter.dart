04173d636d5325eb62b3978d91a573aba0276c6e
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-11 16:04:31 +0600

    Updated Getter And Setter

--- lib/oop/Encapsulation/getter_and_setter.dart
+++ lib/oop/Encapsulation/getter_and_setter.dart
@@ -7,6 +7,7 @@ Setter update the value of property and act as mutator.*/
 
 
 
+/*
 class Student {
   // Private Properties
   String? _firstName;
@@ -44,4 +45,42 @@ void main() {
   // Display the values of the object
   print("Full Name: ${st.fullName}");
   print("Age: ${st.age}");
+}
+*/
+
+
+
+class BankAccount {
+  // Private Property
+  double _balance = 0.0;
+
+  // Getter to read private property _balance
+  double get balance => this._balance;
+
+  // Method to deposit money
+  void deposit(double amount) {
+    this._balance += amount;
+  }
+
+  // Method to withdraw money
+  void withdraw(double amount) {
+    if (this._balance >= amount) {
+      this._balance -= amount;
+    } else {
+      throw new Exception("Insufficient Balance");
+    }
+  }
+}
+
+void main() {
+  // Create an object of BankAccount class
+  BankAccount account = new BankAccount();
+  // Deposit money
+  account.deposit(1000);
+  // Display the balance
+  print("Balance after deposit: ${account.balance}");
+  // Withdraw money
+  account.withdraw(500);
+  // Display the balance
+  print("Balance after withdraw: ${account.balance}");
 }
