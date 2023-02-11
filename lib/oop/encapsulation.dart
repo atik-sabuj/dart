be02138259c3a27143681c929dcde3cdec25197c
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-11 15:19:52 +0600

    Created Encapsulation

---
+++ lib/oop/encapsulation.dart
@@ -0,0 +1,39 @@
+//Encapsulation:
+/*
+//Encapsulation means hiding data within a library, preventing it from outside factors.
+//It helps you control your program and prevent it from becoming too complicated.*/
+
+class Employee {
+  // Private properties
+  int? _id;
+  String? _name;
+
+// Getter method to access private property _id
+  int getId() {
+    return _id!;
+  }
+// Getter method to access private property _name
+  String getName() {
+    return _name!;
+  }
+// Setter method to update private property _id
+  void setId(int id) {
+    this._id = id;
+  }
+// Setter method to update private property _name
+  void setName(String name) {
+    this._name = name;
+  }
+}
+
+void main() {
+  // Create an object of Employee class
+  Employee emp = new Employee();
+  // setting values to the object using setter
+  emp.setId(1);
+  emp.setName("John");
+
+  // Retrieve the values of the object using getter
+  print("Id: ${emp.getId()}");
+  print("Name: ${emp.getName()}");
+}
