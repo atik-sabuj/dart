f13f7be2d9f4248981c224d9932156aa5af7af1d
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-11 14:08:01 +0600

    Created Parameterized Constructor

---
+++ lib/oop/parameterized_constructor.dart
@@ -0,0 +1,50 @@
+//Parameterized Constructor:
+
+/*
+class ClassName {
+  // Instance Variables
+  int? number;
+  String? name;
+  // Parameterized Constructor
+  ClassName(this.number, this.name);
+}*/
+
+
+
+/*
+class Student {
+  String? name;
+  int? age;
+  int? rollNumber;
+  // Constructor
+  Student(this.name, this.age, this.rollNumber);
+}
+void main(){
+  // Here student is object of class Student.
+  Student student = Student("John", 20, 1);
+  print("Name: ${student.name}");
+  print("Age: ${student.age}");
+  print("Roll Number: ${student.rollNumber}");
+}*/
+
+
+
+//Parameterized Constructor With Named Parameters:
+class Student {
+  String? name;
+  int? age;
+  int? rollNumber;
+  // Constructor
+  Student({String? name, int? age, int? rollNumber}) {
+    this.name = name;
+    this.age = age;
+    this.rollNumber = rollNumber;
+  }
+}
+void main(){
+  // Here student is object of class Student.
+  Student student = Student(name: "Sabuj", age: 20, rollNumber: 1);
+  print("Name: ${student.name}");
+  print("Age: ${student.age}");
+  print("Roll Number: ${student.rollNumber}");
+}
