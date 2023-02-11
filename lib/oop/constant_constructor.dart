eb8ec8b168bb4d6a0973902f0fca3a29e034413e
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-11 14:32:53 +0600

    Updated Constant Constructor

--- lib/oop/constant_constructor.dart
+++ lib/oop/constant_constructor.dart
@@ -23,3 +23,42 @@ void main() {
   Point p4 = Point(2, 2);
   print("The p4 hash code is: ${p4.hashCode}");
 }*/
+
+
+
+/*
+class Student {
+  final String? name;
+  final int? age;
+  final int? rollNumber;
+
+  // Constant Constructor
+  const Student({this.name, this.age, this.rollNumber});
+}
+
+void main() {
+  // Here student is object of Student.
+  const Student student = Student(name: "John", age: 20, rollNumber: 1);
+  print("Name: ${student.name}");
+  print("Age: ${student.age}");
+  print("Roll Number: ${student.rollNumber}");
+}*/
+
+
+
+class Car {
+  final String? name;
+  final String? model;
+  final int? price;
+
+  // Constant Constructor
+  const Car({this.name, this.model, this.price});
+}
+
+void main() {
+  // Here car is object of class Car.
+  const Car car = Car(name: "BMW", model: "X5", price: 50000);
+  print("Name: ${car.name}");
+  print("Model: ${car.model}");
+  print("Prize: ${car.price}");
+}
