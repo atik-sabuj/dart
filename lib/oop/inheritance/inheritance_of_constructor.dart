3f145255d7d19f4ba6b27e12c366a2f180fb8c49
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-12 21:29:21 +0600

    Updated Inheritance Of Constructor

--- lib/oop/inheritance/inheritance_of_constructor.dart
+++ lib/oop/inheritance/inheritance_of_constructor.dart
@@ -1,5 +1,6 @@
 //Inheritance Of Constructor:
 
+/*
 class Laptop {
   // Constructor
   Laptop() {
@@ -10,6 +11,109 @@ class Laptop {
 class MacBook extends Laptop {
   // Constructor
   MacBook() {
+    print("MacBook constructor");
+  }
+}
+
+void main() {
+  var macbook = MacBook();
+}*/
+
+
+
+//Inheritance Of Constructor With Parameters:
+/*
+class Laptop {
+  // Constructor
+  Laptop(String name, String color) {
+    print("Laptop constructor");
+    print("Name: $name");
+    print("Color: $color");
+  }
+}
+
+class MacBook extends Laptop {
+  // Constructor
+  MacBook(String name, String color) : super(name, color) {
+    print("MacBook constructor");
+  }
+}
+
+void main() {
+  var macbook = MacBook("MacBook Pro", "Silver");
+}*/
+
+
+
+
+/*
+class Person {
+  String name;
+  int age;
+
+  // Constructor
+  Person(this.name, this.age);
+}
+
+class Student extends Person {
+  int rollNumber;
+
+  // Constructor
+  Student(String name, int age, this.rollNumber) : super(name, age);
+}
+
+void main() {
+  var student = Student("John", 20, 1);
+  print("Student name: ${student.name}");
+  print("Student age: ${student.age}");
+  print("Student roll number: ${student.rollNumber}");
+
+}*/
+
+
+
+
+//Inheritance Of Constructor With Named Parameters:
+/*
+class Laptop {
+  // Constructor
+  Laptop({String? name, String? color}) {
+    print("Laptop constructor");
+    print("Name: $name");
+    print("Color: $color");
+  }
+}
+
+class MacBook extends Laptop {
+  // Constructor
+  MacBook({String? name, String? color}) : super(name: name, color: color) {
+    print("MacBook constructor");
+  }
+}
+
+void main() {
+  var macbook = MacBook(name: "MacBook Pro", color: "Silver");
+}*/
+
+
+
+
+//Calling Named Constructor Of Parent Class:
+class Laptop {
+  // Default Constructor
+  Laptop() {
+    print("Laptop constructor");
+  }
+
+  // Named Constructor
+  Laptop.named() {
+    print("Laptop named constructor");
+  }
+}
+
+class MacBook extends Laptop {
+  // Constructor
+  MacBook() : super.named() {
     print("MacBook constructor");
   }
 }
