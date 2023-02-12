c45a509a2cb42361ba442fb05c56612bfb5bc572
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-12 20:53:49 +0600

    Updated inheritance #2

--- lib/oop/inheritance/inheriance.dart
+++ lib/oop/inheritance/inheriance.dart
@@ -76,6 +76,7 @@ void main() {
 
 
 
+/*
 class Car{
   String? color;
   int? year;
@@ -102,4 +103,31 @@ void main(){
   toyota.price = 2000000;
   toyota.start();
   toyota.showDetails();
+}*/
+
+
+
+
+class Car {
+  // Properties
+  String? name;
+  double? prize;
+}
+
+class Tesla extends Car {
+  // Method to display the values of the properties
+  void display() {
+    print("Name: ${name}");
+    print("Prize: ${prize}");
+  }
+}
+
+void main() {
+  // Create an object of Tesla class
+  Tesla t = new Tesla();
+  // setting values to the object
+  t.name = "Tesla Model 3";
+  t.prize = 50000.00;
+  // Display the values of the object
+  t.display();
 }
