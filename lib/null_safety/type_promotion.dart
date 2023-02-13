413da36f59f72739723550efda0cf8d1acac9c5b
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-13 16:47:43 +0600

    Updated Type promotion

--- lib/null_safety/type_promotion.dart
+++ lib/null_safety/type_promotion.dart
@@ -14,6 +14,7 @@ ii)Promoting from nullable types to non-nullable types*/
 
 
 //Promoting From General Types To Specific Subtypes:
+/*
 void main(){
   Object name = "Atik Sabuj";
 // print(name.length) will not work because Dart doesn't know that name is a String
@@ -22,4 +23,72 @@ void main(){
 // name promoted from Object to String
     print("The length of name is ${name.length}");
   }
+}*/
+
+
+
+
+//Type Promotion:
+/*
+void main(){
+// result is a String
+  String result;
+// result is promoted to a non-nullable type String
+  if(DateTime.now().hour < 12) {
+    result = "Good Morning";
+  } else {
+    result = "Good Afternoon";
+  }
+// display the result
+  print("Result is $result");
+  print("Length of result is ${result.length}");
+}*/
+
+
+
+
+
+
+//Type Promotion With Nullable To Non-Nullable Type:
+/*
+// method to print the length of the text
+void printLength(String? text){
+  if(text == null) {
+    throw Exception("The text is null");
+  }
+  print("Length of text is ${text.length}");
+}
+// main method
+void main() {
+  printLength("Hello Pk");
+}*/
+
+
+
+
+
+//Type Promotion With Nullable Type To Non-Nullable Type:
+
+// importing dart:math library
+import 'dart:math';
+// creating a class DataProvider
+class DataProvider{
+  // creating a method stringorNull
+  String? get stringorNull => Random().nextBool() ? "Hello" : null;
+
+  // creating a method myMethod
+  void myMethod(){
+    String? value = stringorNull;
+    // checking if value String or not
+    if(value is String){
+      print("The length of value is ${value.length}");
+    }else{
+      print("The value is not string.");
+    }
+
+  }
+}
+// main method
+void main() {
+  DataProvider().myMethod();
 }
