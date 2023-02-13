76716281907f5cf7228c7000fdcbf30c9313bd8b
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-13 20:15:24 +0600

    Updated Stream

--- lib/asynchronous/stream.dart
+++ lib/asynchronous/stream.dart
@@ -8,6 +8,7 @@ Stream has one or more listeners, and all listeners will receive the same value.
 
 
 
+/*
 // function that returns a stream
 Stream<String> getUserName() async* {
   await Future.delayed(Duration(seconds: 1));
@@ -24,4 +25,76 @@ void main() async {
   await for (String name in getUserName()) {
     print(name);
   }
+}*/
+
+
+
+
+//Example of async:
+/*
+Future<int> doSomeLongTask() async {
+  await Future.delayed(const Duration(seconds: 2));
+  return 21;
+}main() async {
+  int result = await doSomeLongTask();
+  print(result); // prints '42' after waiting 2 second
 }
+*/
+
+
+
+
+//Example Of async*:
+/*
+Stream<int> countForOneMinute() async* {
+  for (int i = 1; i <= 5; i++) {
+    await Future.delayed(const Duration(seconds: 1));
+    yield i;
+  }
+} main() async {
+  await for (int i in countForOneMinute()) {
+    print(i); // prints 1 to 5, one integer per second
+  }
+}*/
+
+
+
+
+//Example Of yield*:
+/*
+Stream<int> str(int n) async* {
+  if (n > 0) {
+    await Future.delayed(Duration(seconds: 2));
+    yield n;
+    yield* str(n - 2);
+  }
+}
+
+void main() {
+  str(10).forEach(print);
+}
+*/
+
+
+
+
+
+
+
+
+//Example of Stream :
+Stream<int> str(int n) async* {
+  for (var i = 1; i <= n; i++) {
+    await Future.delayed(Duration(seconds: 1));
+    yield i;
+  }
+}
+
+void main() {
+  str(10).forEach(print);
+}
+
+
+
+
+
