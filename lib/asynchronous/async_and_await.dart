f71d6b9e5fd767a666b442d68eee29e8073241f4
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-13 19:59:03 +0600

    Updated Async And Await

--- lib/asynchronous/async_and_await.dart
+++ lib/asynchronous/async_and_await.dart
@@ -5,6 +5,7 @@ You can use the await keyword to get the completed result of an asynchronous exp
 
 
 
+/*
 main() {
   print("Start");
   getData();
@@ -15,6 +16,33 @@ main() {
 void getData() async{
   String data = await middleFunction();
   print(data);
+}
+
+Future<String> middleFunction(){
+  return Future.delayed(Duration(seconds:5), ()=> "Hello");
+}*/
+
+
+
+
+
+
+//Handling Errors:
+main() {
+  print("Start");
+  getData();
+  print("End");
+}
+
+
+void getData() async{
+  try{
+    String data = await middleFunction();
+    print(data);
+  }catch(err){
+    print("Some error $err");
+  }
+
 }
 
 Future<String> middleFunction(){
