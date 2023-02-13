acb40dd1438f6eb7a86fab759f028fc9605b39ac
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-13 16:17:17 +0600

    Created Null Safety

---
+++ lib/null_safety/null_safety.dart
@@ -0,0 +1,25 @@
+//Null Safety:
+/*
+Null safety is a feature in the Dart programming language,
+    that helps developers to avoid null errors. */
+
+
+//Using Null In Variables:
+/*
+void main() {
+  int age = null; // give error
+}*/
+
+
+
+
+//How To Assign Values To Nullable Variables?:
+/*
+void main(){
+// Declaring a nullable variable by using ?
+  String? name;
+// Assigning John to name
+  name = "John";
+// Assigning null to name
+  name = null;
+}*/
