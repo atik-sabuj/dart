85158dc7b5ef169beb4ac533f127d3934623f04c
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-13 13:24:59 +0600

    Updated Mixin

--- lib/oop/mixin.dart
+++ lib/oop/mixin.dart
@@ -18,6 +18,7 @@ class ClassName with Mixin1, Mixin2{
 
 
 
+/*
 mixin ElectricVariant {
   void electricVariant() {
     print('This is an electric variant');
@@ -38,4 +39,75 @@ void main() {
   var car = Car();
   car.electricVariant();
   car.petrolVariant();
+}*/
+
+
+
+
+
+/*
+mixin CanFly {
+  void fly() {
+    print('I can fly');
+  }
 }
+
+mixin CanWalk {
+  void walk() {
+    print('I can walk');
+  }
+}
+
+class Bird with CanFly, CanWalk {
+
+}
+
+class Human with CanWalk {
+
+}
+
+void main() {
+  var bird = Bird();
+  bird.fly();
+  bird.walk();
+
+  var human = Human();
+  human.walk();
+
+}*/
+
+
+
+
+//Syntax Of On Keyword: On Keyword In Mixin :
+abstract class Animal {
+  // properties
+  String name;
+  double speed;
+
+  // constructor
+  Animal(this.name, this.speed);
+
+  // abstract method
+  void run();
+}
+
+// mixin CanRun is only used by class that extends Animal
+mixin CanRun on Animal {
+  // implementation of abstract method
+  @override
+  void run() => print('$name is Running at speed $speed');
+}
+
+class Dog extends Animal with CanRun {
+  // constructor
+  Dog(String name, double speed) : super(name, speed);
+}
+
+void main() {
+  var dog = Dog('My Dog', 25);
+  dog.run();
+}
+
+// Not Possible
+// class Bird with Animal { }
