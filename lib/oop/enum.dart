7298af2133eabb379e6d2cc24ae9abbaf568943f
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-13 12:19:35 +0600

    Updated Enum

--- lib/oop/enum.dart
+++ lib/oop/enum.dart
@@ -12,7 +12,7 @@ enum enumName {
 
 
 
-enum days {
+/*enum days {
   Sunday,
   Monday,
   Tuesday,
@@ -47,4 +47,64 @@ void main() {
       print("Today is Saturday.");
       break;
   }
+}*/
+
+
+
+
+/*enum Gender { Male, Female, Other }
+class Person {
+  // Properties
+  String? firstName;
+  String? lastName;
+  Gender? gender;
+
+  // Constructor
+  Person(this.firstName, this.lastName, this.gender);
+
+  // display() method
+  void display() {
+    print("First Name: $firstName");
+    print("Last Name: $lastName");
+    print("Gender: $gender");
+  }
+}
+
+void main() {
+  Person p1 = Person("Johne", "Dhip", Gender.Male);
+  p1.display();
+
+  Person p2 = Person("Amber", "Heart", Gender.Female);
+  p2.display();
+}*/
+
+
+
+
+/*
+enum Days { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }
+void main() {
+  // Days.values: It returns all the values of the enum.
+  for (Days day in Days.values) {
+    print(day);
+  }
+}*/
+
+
+
+
+enum CompanyType {
+  soleProprietorship("Sole Proprietorship"),
+  partnership("Partnership"),
+  corporation("Corporation"),
+  limitedLiabilityCompany("Limited Liability Company");
+
+  // Members
+  final String text;
+  const CompanyType(this.text);
+}
+
+void main() {
+  CompanyType soleProprietorship = CompanyType.soleProprietorship;
+  print(soleProprietorship.text);
 }
