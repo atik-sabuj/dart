0bf85215ae655c83a8dd74c9296539b466b97406
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-13 19:49:48 +0600

    Created Future

---
+++ lib/asynchronous/future.dart
@@ -0,0 +1,17 @@
+//Future:
+/*
+Future represents a value that is not yet available.
+It is used to represent a potential value, or error, that will be available at some time in the future.*/
+
+
+// function that returns a future
+Future<String> getUserName() async {
+  return Future.delayed(Duration(seconds: 2), () => 'Sabuj Coming');
+}
+
+// main function
+void main() {
+  print("Start");
+  getUserName().then((value) => print(value));
+  print("End");
+}
