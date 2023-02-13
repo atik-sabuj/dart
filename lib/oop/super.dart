88a71f7fab388cbcdc7589dbc57f383456fa3e91
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-13 11:25:33 +0600

    Updated Super

--- lib/oop/super.dart
+++ lib/oop/super.dart
@@ -1,9 +1,10 @@
-//Super:
+//Super Keyword:
 /*
 Super is used to refer to the parent class.
 It is used to call the parent class’s properties and methods.*/
 
 
+/*
 class Laptop {
   //Method
   void show() {
@@ -20,4 +21,108 @@ void main() {
   //Creating an object of the MacBook class
   MacBook macBook = MacBook();
   macBook.show();
+}*/
+
+
+
+//Accessing Super Properties:
+/*class Car {
+  int noOfSeats = 4;
+}
+
+class Tesla extends Car {
+  int noOfSeats = 6;
+
+  void display() {
+    print("No of seats in Tesla: $noOfSeats");
+    print("No of seats in Car: ${super.noOfSeats}");
+  }
+}
+
+void main() {
+  var tesla = Tesla();
+  tesla.display();
+}*/
+
+
+
+
+//Super With Constructor :
+/*
+class Employee {
+  // Constructor
+  Employee(String name, double salary) {
+    print("Employee constructor");
+    print("Name: $name");
+    print("Salary: $salary");
+  }
+}
+
+class Manager extends Employee {
+  // Constructor
+  Manager(String name, double salary) : super(name, salary) {
+    print("Manager constructor");
+  }
+}
+
+void main() {
+  Manager manager = Manager("John", 25000.0);
+}*/
+
+
+
+
+
+//Super With Named Constructor:
+/*
+class Employee {
+  // Named constructor
+  Employee.manager() {
+    print("Employee named constructor");
+  }
+}
+
+class Manager extends Employee {
+  // Named constructor
+  Manager.manager() : super.manager() {
+    print("Manager named constructor");
+  }
+}
+
+void main() {
+  Manager manager = Manager.manager();
+}*/
+
+
+
+
+
+
+//Super With Multilevel Inheritance:
+class Laptop {
+  // Method
+  void display() {
+    print("Laptop display");
+  }
+}
+
+class MacBook extends Laptop {
+  // Method
+  void display() {
+    print("MacBook display");
+    super.display();
+  }
+}
+
+class MacBookPro extends MacBook {
+  // Method
+  void display() {
+    print("MacBookPro display");
+    super.display();
+  }
+}
+
+void main() {
+  var macbookpro = MacBookPro();
+  macbookpro.display();
 }
