8799df960f68e6afd8b9379487012e71d884b378
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-13 20:22:57 +0600

    Created Final Vs Const

---
+++ lib/single_topics/final_vs_const.dart
@@ -0,0 +1,20 @@
+//Final Vs Const:
+/*If you do not want to change the value of a variable,
+ then you can use either final or const in dart*/
+
+
+/*
+void main() {
+  final finalName = "Final John Doe";
+  const constName = "Const John Doe";
+
+  finalName = "Raj"; // Not Possible
+  constName = "Anu"; // Not Possible
+
+  print("Final name is " + finalName);
+  print("Const name is " + constName);
+}
+*/
+
+
+
