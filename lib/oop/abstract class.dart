6b0be07ce5967a6402c6dbfbafbd7c228349bcac
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-13 12:48:25 +0600

    Created Abstract class

---
+++ lib/oop/abstract class.dart
@@ -0,0 +1,59 @@
+//Abstract class:
+/*
+An abstract method is a method that is declared without an implementation.
+It is declared with a semicolon (;) instead of a method body.*/
+
+/*
+abstract class ClassName {
+  //Body of abstract class
+  method1();
+  method2();
+}*/
+
+
+
+
+abstract class Vehicle {
+  // Abstract method
+  void start();
+  // Abstract method
+  void stop();
+}
+
+class Car extends Vehicle {
+  // Implementation of start()
+  @override
+  void start() {
+    print('Car started');
+  }
+
+  // Implementation of stop()
+  @override
+  void stop() {
+    print('Car stopped');
+  }
+}
+
+class Bike extends Vehicle {
+  // Implementation of start()
+  @override
+  void start() {
+    print('Bike started');
+  }
+
+  // Implementation of stop()
+  @override
+  void stop() {
+    print('Bike stopped');
+  }
+}
+
+void main() {
+  Car car = Car();
+  car.start();
+  car.stop();
+
+  Bike bike = Bike();
+  bike.start();
+  bike.stop();
+}
