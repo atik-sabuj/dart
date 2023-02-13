effb454259de93f8302206bfc8af5343f78a3bac
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-13 19:53:53 +0600

    Updated Future

--- lib/asynchronous/future.dart
+++ lib/asynchronous/future.dart
@@ -4,6 +4,7 @@ Future represents a value that is not yet available.
 It is used to represent a potential value, or error, that will be available at some time in the future.*/
 
 
+/*
 // function that returns a future
 Future<String> getUserName() async {
   return Future.delayed(Duration(seconds: 2), () => 'Sabuj Coming');
@@ -14,4 +15,24 @@ void main() {
   print("Start");
   getUserName().then((value) => print(value));
   print("End");
+}*/
+
+
+
+
+
+
+main() {
+  print("Start");
+  getData();
+  print("End");
+}
+
+void getData() async{
+  String data = await middleFunction();
+  print(data);
+}
+
+Future<String> middleFunction(){
+  return Future.delayed(Duration(seconds:5), ()=> "Hello NEW ARRIVEL");
 }
