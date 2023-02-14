e3c145e544e33680c04bd98771f8b34e43b046fa
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-14 22:09:43 +0600

    Updated User Input #2

--- lib/basic/user_input.dart
+++ lib/basic/user_input.dart
@@ -1,14 +1,53 @@
 //User Input:
 import 'dart:io';
+/*
 void main() {
+*/
 /*  print("Enter name:");
   String? name  = stdin.readLineSync();
   print("The entered name is ${name}");
-  print("The entered name length is ${name!.length}");*/
+  print("The entered name length is ${name!.length}");*//*
+
 
 
     print("Enter a floating number:");
     double number = double.parse(stdin.readLineSync()!);
     print("The entered num is $number");
 
+}*/
+
+
+
+
+
+/*void main() {
+    int x = 20;
+
+    print("Enter Your Name: ");
+    String? name = stdin.readLineSync();
+    print("Your Name is $name");
+    print(x);
+}*/
+
+
+
+
+void main() {
+    stdout.write("Enter Your Number One: ");
+    int numberOne = int.parse(stdin.readLineSync()!);
+
+    stdout.write("Enter Your Number Two: ");
+    int numberTwo = int.parse(stdin.readLineSync()!);
+
+    int result = numberOne + numberTwo;
+    double division = numberOne / numberTwo;
+
+    print('Sum: $result');
+    print('division: $division');
 }
+
+
+
+
+
+
